5122855662c3b8dbac9b5cce6f0e31c9e060b296
maazshaikh2079 <124486797+maazshaikh2079@users.noreply.github.com>
2023-06-21 23:55:59 +0530

    Assignment6.py

--- Assignment6.py
+++ Assignment6.py
@@ -102,3 +102,78 @@ del infoDict;
 # Enter name    : Vishal 
 
 # Information 'Dictionary' : {'Roll no.': 116, 'Name': 'Vishal'}
+
+#############################################################################################################################################################
+
+# Info:-
+# Name   : Maaz 
+# Dept   : CO-B
+# Batch  : 4
+# Sub    : Python Assignment-6 (2)
+# Topic  :-
+# 1)Create a function named ds with parameters roll_no and name.
+# 2)Add those parameters in the following data structures:
+# list, tuple, sets and dictionaries
+# 3) After adding values
+# change the values during runtime
+# 4)Delete these data structures
+# Date   : 20-06-2023
+
+# Program:-
+def ds(roll_no,name):
+    global List 
+    global Tuple
+    global Set
+    global Dictionary
+    List=[roll_no,name]
+    Tuple=(roll_no,name)
+    Set={roll_no,name}
+    Dictionary={"Roll no.": roll_no , "Name":name}
+
+
+def printds():
+   
+    print("\nList       : ",List,"\n")
+    print("Tuple      : ",Tuple,"\n")
+    print("Set        : ",Set,"\n")
+    print("Dictionary : ",Dictionary,"\n")
+
+def dsUpdate():
+   print("Updating Data Structures :-\n")
+   rn=int(input("Enter the roll no : "))
+   nm=input("Enter the name    : ")
+   ds(rn,nm)
+   List.append("SYCO-B");
+   List.pop(0)
+   List.insert(0,109)
+  
+   Set.add("SYCO-B")
+   Dictionary.update({"Class":"SYCO-B"})
+
+# Driver code:-
+ds(109,"Maaz")
+printds();
+dsUpdate();
+printds();
+
+# # Output:-
+# List       :  [109, 'Maaz']
+
+# Tuple      :  (109, 'Maaz')
+
+# Set        :  {109, 'Maaz'}
+
+# Dictionary :  {'Roll no.': 109, 'Name': 'Maaz'}
+
+# Updating Data Structures :-
+
+# Enter the roll no : 94
+# Enter the name    : Hassan
+
+# List       :  [109, 'Hassan', 'SYCO-B']
+
+# Tuple      :  (94, 'Hassan')
+
+# Set        :  {'Hassan', 'SYCO-B', 94}
+
+# Dictionary :  {'Roll no.': 94, 'Name': 'Hassan', 'Class': 'SYCO-B'}
